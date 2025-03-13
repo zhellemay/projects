72d817fb4bdcff8e5b48c94da7880ee1bf1994aa
zhellemay <chrizhellemae@gmail.com>
2025-03-13 14:09:10 +0800

    Resolved conflicts

--- Pulla & Suarez Tree.py
+++ Pulla & Suarez Tree.py
@@ -4,6 +4,22 @@ class TreeNode:
         self.left = None
         self.right = None
 
+def swap_level(root, k):
+    level = 1
+    q = []
+    q.append(root)
+    while q:
+        n = len(q)
+        for i in range(n):
+            temp = q.pop(0)
+            if (level+1) % k == 0:
+                temp.left, temp.right = temp.right, temp.left
+            if temp.left != None:
+                q.append(temp.left)
+            if temp.right != None:
+                q.append(temp.right)
+        level += 1
+
 def inorder_traversal(root):
     if root:
         inorder_traversal(root.left)
@@ -22,7 +38,6 @@ def postorder_traversal(root):
         postorder_traversal(root.right)
         print(root.value, end=" ")
 
-
 root = TreeNode(1)
 root.left = TreeNode(2)
 root.right = TreeNode(3)
@@ -31,9 +46,24 @@ root.left.left = TreeNode(4)
 root.right.left = TreeNode(6)
 root.right.right = TreeNode(7)
 
-print("In-order Traversal")
-inorder_traversal(root)
-print("\nPre-order Traversal")
+k=1
+
+print("Pre-order Traversal")
 preorder_traversal(root)
+print("\nIn-order Traversal")
+inorder_traversal(root)
 print("\nPost Order Traversal")
 postorder_traversal(root)
+print("\n ")
+
+print("Swapping Subtrees...")
+
+swap_level(root, k)
+
+print("\nIn-order Traversal After Swap")
+inorder_traversal(root)
+print("\nPre-order Traversal After Swap")
+preorder_traversal(root)
+print("\nPost Order Traversal After Swap")
+postorder_traversal(root)
+
